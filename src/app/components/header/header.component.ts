2ec75fa4aebf138384b7853f2e39c5c9edd9e763
Sharon Brown <bravid11@gmail.com>
2021-01-21 19:38:19 +0200

    header comp

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public username:string = "Guest"
   constructor() { }
 
   ngOnInit(): void {
